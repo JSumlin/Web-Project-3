bcda19718bcf6bedf15835686af68a697b48bef8
tottinguyen1 <144629323+tottinguyen1@users.noreply.github.com>
2023-11-28 22:48:55 -0500

    Add files via upload
    Added a button trigger and audio background music to the HTML file, and added shuffleButton(), startTimer(), stopTimer(), setMoves(), playBackgroundSound(), pauseBackgroundSound(), playWinningSound() to the javascript file. I don't have the function to check whether the game is solved or not, when the game is solved add stopTimer(), pauseBackgroundSound(), and playWinningSpund().

---
+++ fifteen.js
@@ -0,0 +1,129 @@
+let timer;
+let moves = 0;
+let time = 0;
+let bestTime = Number.MAX_SAFE_INTEGER;
+let bestMoves = Number.MAX_SAFE_INTEGER;
+const slidingSound = new Audio("sliding.wav");
+const victorySound = new Audio("victory.mp3")
+// Adds click listener to all moveable pieces
+function addListeners() {
+    var moveablepieces = document.getElementsByClassName("moveablepiece");
+    for(let i=0; i<moveablepieces.length; ++i){
+        moveablepieces[i].addEventListener("click", onClick);
+    }
+    
+}
+
+// Removes all listeners and removes all divs from the moveable piece class
+function removeListeners() {
+    var moveablepieces = document.getElementsByClassName("moveablepiece");
+    while (moveablepieces.length > 0) {
+        moveablepieces[0].removeEventListener("click", onClick);
+        moveablepieces[0].className = moveablepieces[0].className.replace(" moveablepiece", "");
+    }
+}
+
+function onClick(event) {
+    movePiece(event);
+    removeListeners();
+    setMoveablePieces();
+    addListeners();
+
+    setMoves();
+    
+}
+// Moves the appropriate piece to the inactive position and sets its previous position as inactive
+function movePiece(event) {
+    const target = event.currentTarget;
+    const inactiveCell = document.getElementsByClassName("inactive")[0];
+    const piece = target.children[0];
+    inactiveCell.innerHTML = target.innerHTML;
+    target.innerHTML = "";
+    inactiveCell.className = inactiveCell.className.replace("inactive", "active");
+    inactiveCell.className += ` ${piece.id}`;
+    target.className = target.className.replace(` ${piece.id}`, "");
+    target.className = target.className.replace("active", "inactive");
+
+}
+
+// Identifies which pieces should be moveable and adds them to the moveable piece class
+function setMoveablePieces(){
+    const inactiveCell = document.getElementsByClassName("inactive")[0];
+    var inactivePosition =  parseInt(inactiveCell.id.substring(12));
+    var moveablePositions = [inactivePosition - 1, inactivePosition + 1, inactivePosition + 4, inactivePosition - 4];
+    for(var i = 0; i<moveablePositions.length; ++i) {
+        if(moveablePositions[i] > 0 && moveablePositions[i] < 17) {
+            if(inactivePosition % 4 === 0 && moveablePositions[i] === inactivePosition + 1)
+                continue;
+            if(inactivePosition % 4 === 1 && moveablePositions[i] === inactivePosition - 1)
+                continue;
+            if(moveablePositions[i] < 10)
+                moveablePositions[i] = "0" + moveablePositions[i];
+            let moveablePosition = document.getElementById("cellPosition" + moveablePositions[i]);
+            moveablePosition.className += " moveablepiece";
+        }
+    }
+}
+
+// Simulates 100 - 249 clicks to shuffle the gameboard
+function shuffle(){
+    var moveablepieces;
+    var randomPiece;
+    const numOfMoves = getRandomInt(100) + 150;
+    for(let i =0; i<numOfMoves; ++i){
+        moveablepieces = document.getElementsByClassName("moveablepiece");
+        randomPiece = moveablepieces[getRandomInt(moveablepieces.length)];
+        randomPiece.click();
+    }
+}
+// Generates random number up to but not including the max. Has uniform PDF
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// button trigger
+function shuffleButton() {
+    pauseBackgroundMusic();
+    shuffle();
+    startTimer();
+    document.getElementById("moves").innerHTML = '';
+    moves = 0;
+    playBackgroundMusic(); 
+}
+
+//add when game is solved
+function stopTimer(){
+    clearInterval(timer);
+}
+
+
+function startTimer(){
+    clearInterval(timer);
+    time = 0;
+    timer = setInterval(function() {
+        document.getElementById("timer").textContent = time + "s";
+        time++;
+    }, 1000);
+}
+
+function setMoves() {
+    slidingSound.play();
+    moves++;
+    document.getElementById("moves").textContent = moves;
+}
+
+//add when game is solved
+function playWinningSound(){
+    victorySound.play();
+}
+
+function playBackgroundMusic() {
+    const backgroundMusic = document.getElementById("backgroundMusic");
+    backgroundMusic.play();
+}
+
+// add when game is solved
+function pauseBackgroundMusic() {
+    const backgroundMusic = document.getElementById("backgroundMusic");
+    backgroundMusic.pause();
+}
